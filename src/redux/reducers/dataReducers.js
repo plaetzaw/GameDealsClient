d049f14ea065b31621099e8544a11c4d8cf71be0
plaetzaw <plaetzaw@gmail.com>
2020-09-02 16:37:09 -0500

    Finished Reducers and Redux Store

---
+++ src/redux/reducers/dataReducers.js
@@ -0,0 +1,48 @@
+const {
+  GET_FAVORITE_GAMES,
+  SET_GAME_TO_FAVORITES,
+  DELETE_GAME_FROM_FAVORITES,
+  SEARCH_FOR_GAME_BY_TITLE,
+  SEARCH_FOR_DEALS,
+} = require("../actions/actionTypes");
+
+const initialState = {
+  loadingData: true,
+  searchedGames: [],
+  favorites: [],
+  uploads: [],
+};
+
+const dataReducers = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_FAVORITE_GAMES:
+      return {
+        ...state,
+        loadingData: false,
+        favorites: action.payload,
+      };
+    case SET_GAME_TO_FAVORITES:
+      return {
+        ...state,
+        uploads: action.payload,
+      };
+    case DELETE_GAME_FROM_FAVORITES:
+      return {
+        ...state,
+      };
+    case SEARCH_FOR_GAME_BY_TITLE:
+      return {
+        ...state,
+        searchedGames: action.payload,
+      };
+    case SEARCH_FOR_DEALS:
+      return {
+        ...state,
+        searchedGames: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default dataReducers;
