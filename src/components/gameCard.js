149c76a70b2ece55fac23149dddb519b8f3cacf5
plaetzaw <plaetzaw@gmail.com>
2020-09-04 18:49:45 -0500

    isOnSale markup added

--- src/components/gameCard.js
+++ src/components/gameCard.js
@@ -21,9 +21,24 @@ export default class gameCard extends Component {
       metacriticScore,
       steamRatingText,
       steamRatingPercent,
-      steamRAtingCount,
+      steamRatingCount,
       dealRating,
     } = this.props.data;
+
+    let saleMarkup = !isOnSale ? (
+      <>
+        <h3>
+          This game is not currently on sale. It's current price is ${salePrice}
+        </h3>
+      </>
+    ) : (
+      <>
+        <h1>This game is currently on Sale!</h1>
+        Current Price: {salePrice} Normal Price: {normalPrice} Savings %:
+        {savings}
+        {savings}
+      </>
+    );
     return (
       <Card>
         <CardActionArea>
@@ -31,21 +46,21 @@ export default class gameCard extends Component {
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
               {title}
+              <br />
+              <h2>Ratings</h2>
+              Steam: {steamRatingPercent} based on {steamRatingCount} reviews
+              classifying the game as {steamRatingText}
+              <br />
+              Metacritic score: {metacriticScore}/100
             </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
-              Current Price: {salePrice}
-              Normal Price: {normalPrice}
-              Savings %: {savings}
+              <h3>Pricing</h3>
+              {saleMarkup}
             </Typography>
           </CardContent>
         </CardActionArea>
         <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
+          <Button size="small" color="primary"></Button>
         </CardActions>
       </Card>
     );
